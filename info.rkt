15c89b4ff0af4b7fac0687def023b74ff8acea90
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-18 16:37:37 -0500

    Add missing deps.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "misc1"))
-(define build-deps '("racket-doc" "unstable-doc"))
+(define deps '("base" "scribble-lib" "misc1" "unix-socket-lib"))
+(define build-deps '("racket-doc" "unstable-doc" "unix-socket-doc"))
 
 ; vim:set ts=2 sw=2 et:
